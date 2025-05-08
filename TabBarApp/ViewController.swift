fc05db195221324ab312c22fac308ff4627a2cda
Andrey <Rhymes.un.limit@gmail.com>
2025-05-08 23:29:51 +0300

    prepare project

--- TabBarApp/ViewController.swift
+++ TabBarApp/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  TabBarApp
-//
-//  Created by Андрей Терентьев on 08.05.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 

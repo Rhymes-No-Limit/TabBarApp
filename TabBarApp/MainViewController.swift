1e081553abf2ee5e65e7c84c764aaebd287813c5
Andrey <Rhymes.un.limit@gmail.com>
2025-05-08 23:52:08 +0300

    settings VC

--- TabBarApp/MainViewController.swift
+++ TabBarApp/MainViewController.swift
@@ -2,10 +2,20 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let tabBarVC = segue.destination as? UITabBarController
         
+        tabBarVC?.viewControllers?.forEach{ viewController in
+            if let firstVC = viewController as? FirstViewController {
+                firstVC.view.backgroundColor = .systemOrange
+            } else if let secondVC = viewController as? SecondViewController {
+                secondVC.view.backgroundColor = .systemMint
+            } else if let navigationVC = viewController as? UINavigationController {
+                let thirdVC = navigationVC.topViewController as? ThirdViewController
+                thirdVC?.title = "Third Screen"
+                thirdVC?.view.backgroundColor = .systemCyan
+            }
+        }
     }
 
 

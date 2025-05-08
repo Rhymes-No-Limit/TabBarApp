fcdf2b910cc206642138c051f2044cd839721c2d
Andrey <Rhymes.un.limit@gmail.com>
2025-05-08 23:56:38 +0300

    settings tab bar

---
+++ TabBarApp/TabBarViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+    
+
+}
